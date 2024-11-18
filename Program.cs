fdb41751d1eba1d73e79d0f1335b8ac84925bb67
stephen-ross2 <stephen.ross2@outlook.com>
2024-11-18 13:46:09 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿
+namespace Assignment_3_1_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to character counter. Please input your text");
+            string inputString = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("What character do you want to count");
+            char charToCount = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            int characterCount = CountCharacterOccurences(inputString, charToCount);
+
+            Console.WriteLine($"The character {charToCount} appears {characterCount}");
+        }
+
+        static int CountCharacterOccurences(string input, char character)
+        {
+            int count = 0;
+
+            foreach (char c in input)
+            {
+
+                if (c == character)
+
+                {
+                    count++;
+                }
+
+            }
+
+            return count;
+
+        }
+
+    }
+}
